5a2cc4fe0b31a5747e21179ae400ef01e0e25b9f
GisatAZK2 <gisatazk2@gmail.com>
2026-04-08 05:32:36 +0700

    Add section

--- public/index.php
+++ public/index.php
@@ -5,14 +5,17 @@ require_once __DIR__ . "/../routes/web.php";
 
 session_start();
 
-if (isset($_SESSION['role'])) {
-    if ($_SESSION === 'admin') {
-        redirect('/admin-dashboard');
-        exit;
-    } elseif ($_SESSION === 'angota') {
-        redirect('/anggota-dashboard');
-        exit;
-    } 
+$currentUrl = $_SERVER['REQUEST_URI'];
+$role = $_SESSION['role'] ?? null;
+
+if ($role === 'admin' && $currentUrl !== '/admin-dashboard') {
+    redirect('/admin-dashboard');
+    exit;
+}
+
+if ($role === 'anggota' && $currentUrl !== '/dashboard') {
+    redirect('/dashboard');
+    exit;
 }
 
 Router::dispatch();
